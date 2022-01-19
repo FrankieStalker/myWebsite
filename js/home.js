139d1d8d20d15b5b36c3755b439e164d0f7748d7
FrankieStalker <frankiestalker@gmail.com>
2022-01-19 02:43:41 +0000

    First commit

---
+++ js/home.js
@@ -0,0 +1,11 @@
+let i = 0;
+let txt = 'Frankie Stalkers Portfolio';
+let speed = 150;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("titlePage").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
---
+++ js/university.js
@@ -0,0 +1,11 @@
+let i = 0;
+let txt = 'University Projects';
+let speed = 150;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("titlePage").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
