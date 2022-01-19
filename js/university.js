f456cca6229bbe02e709bdd4a5ab14a244276800
FrankieStalker <frankiestalker@gmail.com>
2022-01-19 20:38:41 +0000

    Uni projects addition

--- js/university.js
+++ js/university.js
@@ -9,3 +9,18 @@ function typeWriter() {
     setTimeout(typeWriter, speed);
   }
 }
+
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}    
+  x[myIndex-1].style.display = "block";  
+  setTimeout(carousel, 5000); // Change image every 2 seconds
+}
